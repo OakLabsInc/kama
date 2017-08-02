a238bada2b2cd2005e4aac7a5ad7b1b0869c9863
Jeremy Grosser <jeremy@synack.me>
2017-08-02 03:32:06 -0700

    Release 1.0.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
         name='kama',
-        version='1.0.0',
+        version='1.0.1',
         description='A truth database',
         url='https://kama.sh/',
         author='Jeremy Grosser',
