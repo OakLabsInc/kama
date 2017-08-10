361cb77791368cc0cbad649dd867ba558e864ca1
Jeremy Grosser <jeremy@oaklabs.is>
2017-08-10 06:59:37 -0700

    Release 1.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
         name='kama',
-        version='1.1.0',
+        version='1.1.1',
         description='A truth database',
         url='https://kama.sh/',
         author='Jeremy Grosser',
