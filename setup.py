6339db340a75c7682ce70a330ebf454a24db322a
Jeremy Grosser <jeremy@oaklabs.is>
2017-08-25 06:53:16 -0700

    Tagging 1.2.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
         name='kama',
-        version='1.2.1',
+        version='1.2.2',
         description='A truth database',
         url='https://kama.sh/',
         author='Jeremy Grosser',
