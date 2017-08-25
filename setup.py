da818dc2aa8e06d85e6be0af2106f907e56c7bef
Jeremy Grosser <jeremy@oaklabs.is>
2017-08-25 08:37:34 -0700

    Taggin 1.2.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from setuptools import setup
 
 setup(
         name='kama',
-        version='1.2.3',
+        version='1.2.4',
         description='A truth database',
         url='https://kama.sh/',
         author='Jeremy Grosser',
